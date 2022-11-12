55a8a3a0d7847eea3ed1786eacf2ce1184175129
veluvj <veluvijay1804@gmail.com>
2022-11-12 14:47:21 +0530

    feat: removed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -68,7 +68,7 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
+  const [setGitHubInfo] = useState({
     stars: null,
     forks: null,
   });
@@ -105,10 +105,10 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <p>
+          <div>Made with ❤</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -119,8 +119,8 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
-        </a>
+          )} */}
+        </p>
       </StyledCredit>
     </StyledFooter>
   );
